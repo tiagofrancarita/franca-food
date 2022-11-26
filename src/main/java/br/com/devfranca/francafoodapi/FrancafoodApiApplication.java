f4514d94cebeb9db372fb1a25d7b2bf4b87be424
Tiago França <tiagofranca.rita@gmail.com>
2022-11-25 23:34:37 -0300

    Commit initial

---
+++ src/main/java/br/com/devfranca/francafoodapi/FrancafoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.devfranca.francafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrancafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devfranca/francafoodapi/FrancafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devfranca.francafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
